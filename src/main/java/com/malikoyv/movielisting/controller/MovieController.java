75a92b964e6e1e968a8b00838d9d45cf9341b451
Yehor Malikov <luziferweed@gmail.com>
2024-06-07 09:29:22 +0200

    add get mapping by id

--- src/main/java/com/malikoyv/movielisting/controller/MovieController.java
+++ src/main/java/com/malikoyv/movielisting/controller/MovieController.java
@@ -2,12 +2,15 @@ package com.malikoyv.movielisting.controller;
 
 import com.malikoyv.movielisting.model.Movie;
 import com.malikoyv.movielisting.repos.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/movies")
 @RestController
@@ -18,5 +21,15 @@ public class MovieController {
     @GetMapping("/getAll")
     public List<Movie> getAllMovies(){
         return repo.findAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public Optional<Movie> getMovieByID(@PathVariable("id") ObjectId id) throws Exception {
+        try {
+            return repo.findById(id.toString());
+
+        } catch (Exception e) {
+            throw new Exception("Error in getMovieByID");
+        }
     }
 }
